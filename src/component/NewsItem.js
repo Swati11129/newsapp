010deb0bbf348feda95e0e3469f7f5b9ae28c881
Swati <swati11129@gmail.com>
2023-03-28 01:08:03 +0530

    done

---
+++ src/component/NewsItem.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const NewsItem =(props)=>{
+    
+    let {title, description, imageUrl, newsUrl, author, date, source} = props;
+    return (
+      <div className='my-3'>
+        <div className="card">
+          <div style={
+            {
+              display:'flex',
+              justifyContent:'flex-end',
+              position:'absolute',
+              right:'0'
+            }
+          } >
+
+           <span  className="badge rounded-pill text-bg-danger  " >{source}</span>
+          </div>
+            <img src={imageUrl?imageUrl:"https://www.elegantthemes.com/blog/wp-content/uploads/2021/02/facebook-paid-advertising-featured-image.jpg"} className="card-img-top" alt="..." />
+        <div className="card-body">
+            <h5 className="card-title">{title} </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {author? author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+            <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More </a>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+export default NewsItem
